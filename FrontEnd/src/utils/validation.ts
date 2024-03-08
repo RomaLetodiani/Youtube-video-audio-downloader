398b98f73f4b6e61ec81063209c41a3ea189df0a
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-08 05:01:31 +0400

    Created Backend Branch

--- FrontEnd/src/utils/validation.ts
+++
@@ -1,4 +0,0 @@
-export const isValidYouTubeUrl = (url: string) => {
-  const pattern = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=.*$/
-  return pattern.test(url)
-}
--- FrontEnd/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Youtube-video-downloader',
-  plugins: [react()],
-})
