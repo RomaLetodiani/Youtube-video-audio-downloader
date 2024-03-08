14383141b947dc345f788d56193cbf41b97547ad
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-09 03:10:41 +0400

    Update vite.config.ts

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Youtube-video-downloader',
+  base: '/Youtube-video-audio-downloader',
   plugins: [react()],
 })
